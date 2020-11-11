ac2fbe7cb36cc763118c28342f740940b4b29f62
Brezo <brezo.al@gmail.com>
2020-11-11 16:18:37 +0100

    Created components task and searchbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchboxComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
